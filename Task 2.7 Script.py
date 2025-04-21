39adbea44aebb3da94853ec26be0dba57c70cfc1
lgreer929 <lisa.greer@comcast.net>
2025-04-20 22:13:36 -0600

    Adding Updated Script

--- Task 2.7 Script.py
+++ Task 2.7 Script.py
@@ -3,6 +3,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly as plt
 import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
